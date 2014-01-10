26c351756a148068323a6d6828ce1a08cde25f9b
唐鳳 <audreyt@audreyt.org>
2014-01-11 02:03:54 +0800

    Point to air.html; easier to demo among twgeojson

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ prjCtrl = function($scope, $http, $element){
       source: 'https://github.com/4movement/movement',
       img: 'movement.jpg'
     }, {
-      name: '台灣行政區視覺化',
-      homepage: 'http://g0v.github.io/twgeojson/',
+      name: '空氣品質即時指標',
+      homepage: 'http://g0v.github.io/twgeojson/air.html',
       source: 'https://github.com/g0v/twgeojson/',
       img: 'twgeojson.jpg'
     }, {
